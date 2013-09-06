0f3e00f723f7adf410a1ccbe66d675b51b7085c5
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2013-09-05 23:17:14 -0500

    Bug Fix: fixed stupid spelling error

--- tasks/main.js
+++ tasks/main.js
@@ -9,7 +9,7 @@ module.exports = function(grunt){
         var options = grunt.config('coveralls.options');
         options.filepath = ".";
         options.service_name =  'travis-ci';
-        options.sersvice_jobj_id = process.env.TRAVIS_JOB_ID;
+        options.service_job_id = process.env.TRAVIS_JOB_ID;
         var lcov_path = glob.sync(options.coverage_dir + "/**/lcov.info")[0];
         var input = fs.readFileSync(lcov_path).toString();
 
