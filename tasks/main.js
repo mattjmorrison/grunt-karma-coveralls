13c9b3782fb8996c370cf50348ee553600851428
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2013-09-06 23:00:34 -0500

    Feature: Added support for multiple ci platforms

--- tasks/main.js
+++ tasks/main.js
@@ -1,33 +1,32 @@
 module.exports = function(grunt){
     var fs = require('fs');
     var glob = require('glob');
-    var sendToCoveralls = require('coveralls/lib/sendToCoveralls');
-    var convertLcovToCoveralls = require('coveralls/lib/convertLcovToCoveralls');
+    var coveralls = require('coveralls/lib/index');
 
     grunt.task.registerTask('coveralls', 'Coveralls coverage with Karma', function(){
         var done = this.async();
-        var options = grunt.config('coveralls.options');
-        options.filepath = ".";
-        options.service_name =  'travis-ci';
-        options.service_job_id = process.env.TRAVIS_JOB_ID;
-        var lcov_path = glob.sync(options.coverage_dir + "/**/lcov.info")[0];
+        var gruntOptions = grunt.config('coveralls.options');
+        var lcov_path = glob.sync(gruntOptions.coverage_dir + "/**/lcov.info")[0];
         var input = fs.readFileSync(lcov_path).toString();
 
-        convertLcovToCoveralls(input, options, function(err, postData){
-            if (err){
-                done();
-                throw err;
-            }
-            sendToCoveralls(postData, function(err, response, body){
+        coveralls.getBaseOptions(function(err, options){
+            options.filepath = ".";
+            coveralls.convertLcovToCoveralls(input, options, function(err, postData){
                 if (err){
                     done();
                     throw err;
                 }
-                if (response.statusCode >= 400){
+                coveralls.sendToCoveralls(postData, function(err, response, body){
+                    if (err){
+                        done();
+                        throw err;
+                    }
+                    if (response.statusCode >= 400){
+                        done();
+                        throw "Bad response: " + response.statusCode + " " + body;
+                    }
                     done();
-                    throw "Bad response: " + response.statusCode + " " + body;
-                }
-                done();
+                });
             });
         });
     });
